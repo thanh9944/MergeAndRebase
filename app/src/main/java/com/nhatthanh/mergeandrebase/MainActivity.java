4f55dad8ce37376f8c0d23abfa88ce31aa53f9dc
Le Nhat Thanh <thanhdubai9944@gmail.com>
2022-08-19 09:26:54 +0700

    add Buton on MainActivity

--- app/src/main/java/com/nhatthanh/mergeandrebase/MainActivity.java
+++ app/src/main/java/com/nhatthanh/mergeandrebase/MainActivity.java
@@ -3,6 +3,7 @@ package com.nhatthanh.mergeandrebase;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
 
         //c1
         //c2
-
+        Button button;
         //b1
         //b2
     }
